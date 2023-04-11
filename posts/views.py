af35630581df86b40937a898b2142ce91da7817e
YOONJAHYUN <dbswkgus323@gmail.com>
2023-04-12 02:33:30 +0900

    0412 Create

---
+++ posts/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
+
+
+# Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'posts/index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form = form.save()
+            return redirect('post:index')
+    else:
+        form = PostForm()
+
+    context = {'form':form}
+    return render(request, 'posts/create.html', context) 
